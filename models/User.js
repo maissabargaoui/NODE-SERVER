6c9e575c1c48263121a4d6eeeb6de2968c4b2153
mayssabargaoui1 <94033311+mayssabargaoui1@users.noreply.github.com>
2021-12-12 01:50:44 +0100

    Update User Schema

--- models/User.js
+++ models/User.js
@@ -5,11 +5,27 @@ const UserSchema = new mongoose.Schema({
         name: { type: String },
         email: { type: String },
         address: { type: String },
+        latitude: { type: Number },
+        longitude: { type: Number },        
         password: { type: String },
         phone: { type: String },
+        idPhoto: { type: String },
         role: { type: String },
         isVerified: { type: Boolean}
-});
+        courses: [{
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Course"
+                }],
+
+                favorites: [{
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Favorite"
+                }]
+        },
+        {
+                timestamps: { currentTime: () => Date.now() }
+        }
+);
 
 //we want to export this file to the server so it can see the schema
 //the data is the student we made
